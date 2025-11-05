8fa6482100601ba4a6e4300668ffb2872fbc5ef5
Jan Ondrej <jan.ondrej@gmail.com>
2025-11-05 18:02:26 +0000

    iOS build

--- iOS/build_plugin.sh
+++ iOS/build_plugin.sh
@@ -17,8 +17,8 @@ xcodebuild build \
     VALID_ARCHS=x86_64 \
     ARCHS=x86_64 \
     -configuration "Release" \
-    HEADER_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/bundle-apple-universal-ios/ffmpeg/include" \
-    LIBRARY_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/bundle-apple-universal-ios/ffmpeg/lib" \
+    HEADER_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/apple-ios-arm64-simulator/ffmpeg/include ../../shared/thirdparty/" \
+    LIBRARY_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/apple-ios-arm64-simulator/ffmpeg/lib" \
     OTHER_CFLAGS="-DDISABLE_LOGGING -fembed-bitcode" \
     OTHER_LDFLAGS="-framework CoreFoundation -framework VideoToolbox -framework CoreVideo -framework CoreMedia -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lswscale -lswresample -llibz -libbz2"
 
@@ -32,18 +32,18 @@ xcodebuild build \
     VALID_ARCHS=arm64 \
     ARCHS=arm64 \
     -configuration "Release" \
-    HEADER_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/bundle-apple-universal-ios/ffmpeg/include" \
-    LIBRARY_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/bundle-apple-universal-ios/ffmpeg/lib" \
+    HEADER_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/apple-ios-arm64/ffmpeg/include ../../shared/thirdparty/" \
+    LIBRARY_SEARCH_PATHS="${FFMPEG_KIT}/prebuilt/apple-ios-arm64/ffmpeg/lib" \
     OTHER_CFLAGS="-DDISABLE_LOGGING -fembed-bitcode" \
     OTHER_LDFLAGS="-framework CoreFoundation -framework VideoToolbox -framework CoreVideo -framework CoreMedia -lavcodec -lavdevice -lavfilter -lavformat -lavutil -lswscale -lswresample -llibz -libbz2"
 echo "DEVICE BUILD COMPLETE\n"
 
-mkdir -p ../VologramsToolkit/Plugins/iOS/
+mkdir -p ../VologramsPlayer/Runtime/Plugins/iOS/
 
 lipo -create \
     ./vol_unity_lib_ios/build/Release-iphoneos/libvol_unity_lib_ios.a \
     ./vol_unity_lib_ios/build/Release-iphonesimulator/libvol_unity_lib_ios.a \
-    -output "../VologramsToolkit/Plugins/iOS/libvolplayer.a"
+    -output "../VologramsPlayer/Runtime/Plugins/iOS/libvolplayer.a"
 echo "UNIVERSAL LIB CREATED\n"
 
 rm -r ./vol_unity_lib_ios/build
